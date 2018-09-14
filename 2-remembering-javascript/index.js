7b03276fb34f67984598d8dab94d185517f76d97
Alexey18 <37097835+Alexey18@users.noreply.github.com>
2018-09-14 10:24:25 +0300

    Update index.js

--- 2-remembering-javascript/index.js
+++ 2-remembering-javascript/index.js
@@ -1,4 +1,4 @@
-/*const numbers = process.argv.slice(2);
+const numbers = process.argv.slice(2);
 let summ = 0;
 let average = 0;
 
@@ -10,16 +10,3 @@ for (i = 0; i < numbers.length; i++){
 console.log(numbers);
 console.log(summ);
 console.log(average);
-*/
-
-
-function pangram(str){
-    let list = [];
-    for (i = 0, i < str.length, i++){
-        list.push(str[i]);
-    }
-    list = unique(list);
-    console.log(list.length);
-}
-
-pangram('The quick brown fox jumps over the lazy dog')
